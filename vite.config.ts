5e2d10135839fb7be54cf8389b8d3df406fac30d
qianye <qianye@gaoding.com>
2021-02-19 18:03:41 +0800

    feat: update pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/zelda-words/',
+
     build: {
         outDir: 'docs',
     },
+
     plugins: [vue()],
 });
